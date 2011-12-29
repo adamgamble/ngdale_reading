4827520f306f5cb4c41cece504dfa623c314b93d
Adam Gamble <adamgamble@gmail.com>
2011-12-29 15:34:10 -0600

    added daily reading

--- app/models/scheduled_sms_alert.rb
+++ app/models/scheduled_sms_alert.rb
@@ -15,4 +15,13 @@ class ScheduledSmsAlert < ActiveRecord::Base
         transition [:cancelled, :authorized, :requested] => :authorized
       end
     end
+
+    def alert_of_todays_reading
+      @daily_reading = DailyReading.first
+      TwilioClient.client.account.sms.messages.create(
+        :from => "12052891112",
+        :to => number,
+        :body => "Today's Readings - Daily: #{@daily_reading.daily_reading} - Weekly: #{@daily_reading.weekly_reading} - Monthly: #{@daily_reading.monthly_reading}"
+      )
+    end
 end
