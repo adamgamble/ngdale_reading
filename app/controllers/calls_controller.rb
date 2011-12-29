3962219698377e91247bbd8f798ad2a86f6aec4f
Adam Gamble <adamgamble@gmail.com>
2011-12-29 15:13:45 -0600

    dialy reading

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,7 +1,9 @@
 class CallsController < ApplicationController
   def create
+    @daily_reading = DailyReading.first
     response = Twilio::TwiML::Response.new do |r|
-      r.Say 'hello there', :voice => 'woman'
+      r.Say 'Welcome to the North Gardendale Bible Reading Hotline', :voice => 'woman'
+      r.Say "Today's verses are #{@daily_reading.daily_reading}", :voice => 'woman'
     end
 
     render :text => response.text
