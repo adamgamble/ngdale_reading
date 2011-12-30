7462993af4c2f214a253626b3f1ced3da4785063
Adam Gamble <adamgamble@gmail.com>
2011-12-29 21:17:27 -0600

    fixed a 500

--- app/models/daily_reading.rb
+++ app/models/daily_reading.rb
@@ -1,6 +1,6 @@
 class DailyReading < ActiveRecord::Base
 
-  def appropriate_reading
+  def self.appropriate_reading
     @daily_reading = DailyReading.where(:date => Date.today)
     @daily_reading = DailyReading.first unless @daily_reading
     @daily_reading
