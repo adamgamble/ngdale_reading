5a9678187f86d5a71dfc2e4fc0af94cbe5945181
Adam Gamble <adamgamble@gmail.com>
2011-12-29 21:37:14 -0600

    added gb

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -2,8 +2,8 @@ class CallsController < ApplicationController
   def create
     @daily_reading = DailyReading.appropriate_reading
     response = Twilio::TwiML::Response.new do |r|
-      r.Say 'Welcome to the North Gardendale Bible Reading Hotline.', :voice => 'woman'
-      r.Say "Today's verses are #{@daily_reading.daily_reading}. The Weekly Verses are #{@daily_reading.weekly_reading}. The Monthly Verses are #{@daily_reading.monthly_reading}.", :voice => 'woman', :loop => 5
+      r.Say 'Welcome to the North Gardendale Bible Reading Hotline.', :voice => 'woman', :language => "en-gb"
+      r.Say "Today's verses are #{@daily_reading.daily_reading}. The Weekly Verses are #{@daily_reading.weekly_reading}. The Monthly Verses are #{@daily_reading.monthly_reading}.", :voice => 'woman', :loop => 5, :language => "en-gb"
     end
 
     render :text => response.text
