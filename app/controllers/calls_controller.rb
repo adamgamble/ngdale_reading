127bf40418bdd78e88a9bb449d262b569ef0b6a2
Adam Gamble <adamgamble@gmail.com>
2012-01-02 01:41:31 -0600

    added - to through

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -3,7 +3,7 @@ class CallsController < ApplicationController
     @daily_reading = DailyReading.appropriate_reading
     response = Twilio::TwiML::Response.new do |r|
       r.Say 'Welcome to the North Gardendale Bible Reading Hotline.', :voice => 'woman', :language => "en-gb"
-      r.Say "Today's verses are #{@daily_reading.daily_reading}. The Weekly Verses are #{@daily_reading.weekly_reading}. The Monthly Verses are #{@daily_reading.monthly_reading}.", :voice => 'woman', :loop => 5, :language => "en-gb"
+      r.Say "Today's verses are #{@daily_reading.daily_reading.gsub("-"," through ")}. The Weekly Verses are #{@daily_reading.weekly_reading.gsub("-"," through ")}. The Monthly Verses are #{@daily_reading.monthly_reading.gsub("-"," through ")}.", :voice => 'woman', :loop => 5, :language => "en-gb"
     end
 
     render :text => response.text
